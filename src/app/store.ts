9422ac7ccc2773ebf85fd56563c3ca61c2691c50
Tantely <tantely.and@onja.org>
2021-09-16 11:31:16 +0300

    deleted counter slice

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,8 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit'
-import counterReducer from '../features/counter/counterSlice'
 import gameReducer from '../features/slices/GameSlice'
 
 export const store = configureStore({
 	reducer: {
-		counter: counterReducer,
 		gameState: gameReducer,
 	},
 })
